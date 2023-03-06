8693dfdc116486367f2eaa9c893a634fb3adf8cf
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-03-06 15:27:46 -0300

    new build, added mobile menu and fixed toutch buttons

--- src/web/src/components/header/header.tsx
+++ src/web/src/components/header/header.tsx
@@ -2,7 +2,7 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-import { FC, Key, useState } from "react";
+import { FC, Key, useState, useEffect } from "react";
 import styleModule from "./header.module.css";
 
 import { FaLaptopCode } from "react-icons/fa";
@@ -11,10 +11,41 @@ import CustomButton from "../customButton/customButton";
 import ButtonGroup from "../buttonGroup/buttonGroup";
 import ConnectionComponent from "./connectionComponent/connectionComponent";
 
+type OrientationType = "landscape" | "portrait";
+
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
     const [currentActive, setCurrentActive] = useState<Key>("Mapa");
+    const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+
+    const getMobileMenuClassName = (): string => {
+        if (menuIsOpen) return styleModule.mobile_menu_open;
+        return styleModule.mobile_menu;
+    };
+
+    const toggleButton = () => {
+        setMenuIsOpen(!menuIsOpen);
+    };
+
+    const orientationChanged = () => {
+        setMenuIsOpen(false);
+    };
+
+    useEffect(() => {
+        window
+            .matchMedia("(orientation: portrait)")
+            .addEventListener("change", (e) => {
+                const portrait = e.matches;
+
+                if (portrait) {
+                    orientationChanged();
+                } else {
+                    orientationChanged();
+                }
+            });
+    }, []);
+
     return (
         <div className={styleModule.header_div} data-testid="header">
             <CustomButton
@@ -24,8 +55,55 @@ const Header: FC<HeaderProps> = () => {
                 preIcon={<MdMenu />}
                 iconSize={"var(--font_l)"}
                 color={"var(--font_color_inverted)"}
+                clickCallBack={toggleButton}
             />
             <div className={styleModule.logo_name}>VIROTE</div>
+            <div className={getMobileMenuClassName()}>
+                <CustomButton
+                    toolTip="Mapa"
+                    key={"Mapa"}
+                    buttonKey={"Mapa"}
+                    postIcon={<MdMap />}
+                    color={"white"}
+                    iconSize="var(--font_s)"
+                    className=""
+                >
+                    Mapa
+                </CustomButton>
+                <CustomButton
+                    toolTip="Inspecionar"
+                    key={"Inspec."}
+                    buttonKey={"Inspec."}
+                    postIcon={<FaLaptopCode />}
+                    color={"white"}
+                    iconSize="var(--font_s)"
+                    className=""
+                >
+                    Inspec.
+                </CustomButton>
+                <CustomButton
+                    toolTip="Configurações"
+                    key={"Config."}
+                    buttonKey={"Config."}
+                    postIcon={<MdSettings />}
+                    color={"white"}
+                    iconSize="var(--font_s)"
+                    className=""
+                >
+                    Config.
+                </CustomButton>
+                <CustomButton
+                    toolTip="Sobre"
+                    key={"Sobre"}
+                    buttonKey={"Sobre"}
+                    postIcon={<MdInfo />}
+                    color={"white"}
+                    iconSize="var(--font_s)"
+                    className=""
+                >
+                    Sobre
+                </CustomButton>
+            </div>
             <ButtonGroup
                 className={styleModule.button_group}
                 currentActive={currentActive}
